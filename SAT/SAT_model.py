41ba0041f734329a99cfd5518da967f24ea47305
arisaghafari <arisaghafari68@gmail.com>
2024-06-29 17:31:10 +0200

    initiate SAT model

---
+++ SAT/SAT_model.py
@@ -0,0 +1,22 @@
+from z3 import *
+from typing import List
+
+
+"""
+    SAT solver
+    :param m: Number of couriers
+    :param l: List of carriable weight by each courier
+    :param n: Number of packages
+    :param s: List of weights of the packages
+    :param D: Matrix of packages distances
+    :return: Minimized distance, found solution, computation time and iterations number
+"""
+def multiple_couriers(
+    m: int, 
+    l: List[int],
+    n: int, 
+    s: List[int],
+    D: List[List[int]]):
+
+
+    return 0
