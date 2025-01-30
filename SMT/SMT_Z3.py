b940ae10e679f1778abeec0d04bb615f99ca9c71
arisaghafari <arisaghafari68@gmail.com>
2025-01-30 22:20:08 +0100

    add implied constraint

--- SMT/SMT_Z3.py
+++ SMT/SMT_Z3.py
@@ -2,9 +2,10 @@ from z3 import *
 import numpy as np
 from time import time
 from SMT.utils import *
+from SMT.additional_constraints import *
 from utils import *
 
-def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
+def SMT(shared_list, m, n, l, s, D, sym_breaking=False, imp_cons=True):
 
   start_time = time()
 
@@ -12,29 +13,26 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
   ITEMS = range(n)
 
   ####################################### DECISION VARIABLES #######################################
-  # main decision variable: x[i,j] = k mean that the i-th courier is in j at time k
+  # main decision variable: x[i,j] = k mean that the i-th courier collect the j-th item at time k
   X = [[Int(f'X_{i}_{j}') for j in ITEMS]for i in COURIERS]
 
-  # variable for distance calculation
+  # stores the total distance traveled by each courier.
   dist = [Int(f'dist_{i}') for i in COURIERS]
 
-  # variable for loads calculation
+  # stores the total load of items collected by each courier
   load = [Int(f'load_{i}') for i in COURIERS]
 
-  # the number of items that are delivered by each courier
+  # the number of items that are collected by each courier
   count = [Int(f'count_{i}') for i in COURIERS]
 
   solver = Solver()
   ####################################### CONSTRAINTS #######################################
 
-  # Assignment Constraint
+  # Each item is collected by exactly one courier
   for j in ITEMS:
     solver.add(Sum([If(X[i][j] > 0, 1, 0) for i in COURIERS]) == 1)
 
-  for i in COURIERS:
-    solver.add(Sum([If(X[i][j] > 0, 1, 0) for j in ITEMS]) >= 1)
-
-  # Load Constraint
+  # The total weight of items collected by each courier must not exceed the courier's load capacity
   for i in COURIERS:
     load[i] = Sum([If(X[i][j] > 0, s[j], 0) for j in ITEMS])
     solver.add(load[i] <= l[i])
@@ -44,11 +42,7 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
 
     count[i] = Sum([If(X[i][j] > 0, 1, 0) for j in ITEMS])
     
-    # balance item distribution
-    max_item = int(n / m) + 1
-    solver.add(count[i] <= max_item)
-
-    #solver.add(count[i] == c)
+    
     # Distance from origin to the first item in the route
     dist_start = Sum([If(X[i][j] == 1, D[n][j], 0) for j in ITEMS])
 
@@ -67,17 +61,20 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
     # Add the constraint for the total distance of courier i
     dist[i] = dist_expr
 
+  # X array boundries in k value
   for i in COURIERS:
     for j in ITEMS:
       solver.add(And(X[i][j] >= 0, X[i][j] <= count[i]))
 
-  # Each items that is delivered in different time(k) by i th couriesr
+  # Each item is delivered at a different time(k) by i-th couriesr
   for i in COURIERS:
     items = [If(X[i][j] > 0, X[i][j], -j) for j in ITEMS]
     solver.add(Distinct(items))
 
   if sym_breaking:
-    add_sb_constraints(m, n, l, s, D, solver, X)
+    add_sb_constraints(m, n, l, solver, X)
+  if imp_cons:
+    add_implied_constraints(m, n, solver, X, count)
 
   ####################################### OBJECTIVE FUNCTION ######################################
 
@@ -86,23 +83,12 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
 
   ######################################## LOWER AND UPPER BOUNDS ########################################
 
-  max_distances = [max(D[i][:-1]) for i in ITEMS]
-  # print(f"before sort : {max_distances}")
-  max_distances.sort()
-  # print(f"after sort : {max_distances}")
-  
-  #upper_bound = sum(max_distances[m:]) + max(D[n]) + max([D[j][n] for j in ITEMS])
   upper_bound = calculate_upper_bound(m, n, l, s, D)
   
-  # lower_bound_distance = max([D[n][j] + D[j][n] for j in ITEMS])
   lower_bound_distance = calculate_lower_bound(n, D)
-  print(f"lower bound distance : {lower_bound_distance}")
-  print(f"upper bound distance : {upper_bound}")
 
   solver.add(obj >= lower_bound_distance)
   solver.add(obj <= upper_bound)
-
-  #print(lower_bound_distance)
 
   ############################################# SOLVE #############################################
   solver.set("timeout", 300000)
@@ -128,7 +114,9 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
     result_objective = model.evaluate(obj)
 
     result_X_final = result_X
-
+    # index1, index2 = add_sb_constraints(m, n, l, solver, X)
+    # print(f"result X index 1: {result_X[index1]}")
+    # print(f"result X index 2: {result_X[index2]}")
     final_value = result_objective
     solver.add(obj < result_objective)
 
@@ -137,8 +125,9 @@ def SMT(shared_list, m, n, l, s, D, sym_breaking=False):
 
     courier_path = courier_path_from_result(result_count_final, result_X_final
                                             , COURIERS, ITEMS)
+    # print(f"courier path : {courier_path}")
     print(f"obj : {final_value}")
     shared_list.append((courier_path, final_value.as_long(), final_time, optimal))
 
-    print(f"shared_list length : {len(shared_list)}")
+    # print(f"shared_list length : {len(shared_list)}")
     #return courier_path, final_value, final_time, optimal
--- utils.py
+++ utils.py
@@ -1,3 +1,65 @@
+
+# def assign_items(courier_capacity, available_items, S, D, depot_idx):
+  
+#     sorted_items = sorted(available_items, key=lambda j: D[j][depot_idx], reverse=True)
+#     assigned_items = []
+#     total_size = 0
+    
+#     for j in sorted_items:
+#         if total_size + S[j] <= courier_capacity:
+#             assigned_items.append(j)  
+#             total_size += S[j]
+   
+#     remaining_items = [item for item in available_items if item not in assigned_items]
+#     return assigned_items, remaining_items
+
+# def calculate_upper_bound(m, n, L, S, D):
+#     depot_idx = n  
+#     available_items = list(range(n))  
+#     print(available_items)
+#     upper_bounds = []
+    
+#     for i in range(m):
+#         if not available_items:
+#             break  
+        
+        
+#         assigned_items, available_items = assign_items(
+#             L[i], available_items, S, D, depot_idx
+#         )
+#         print(assigned_items)
+#         if not assigned_items:
+#             continue  
+        
+        
+#         if len(assigned_items) == 1:
+           
+#             tsp_bound = 2*max(D[depot_idx][assigned_items[0]],  D[depot_idx][assigned_items[0]])
+#         else:
+           
+#             tsp_bound = 0
+#             for item in assigned_items:
+                
+#                 tsp_bound += 2*max(D[depot_idx][item] , D[item][depot_idx])
+
+        
+#         upper_bounds.append(tsp_bound)
+    
+   
+#     upper_bound_final = max(upper_bounds) if upper_bounds else 0
+    
+    
+#     print(f"Final Upper Bound: {upper_bound_final})")
+#     return upper_bound_final
+
+
+
+def calculate_lower_bound(n, D):
+
+    
+    lower_bound = max(D[j][n] + D[n][j] for j in range(n))
+    return lower_bound
+
 def calculate_upper_bound(m, n, L, S, D):
  
     def assign_items(courier_capacity):
@@ -33,18 +95,13 @@ def calculate_upper_bound(m, n, L, S, D):
         upper_bounds.append(min(tsp_bound, worst_case_tour))
 
     # Step 3: Return the maximum upper bound across all couriers
-    uppper_bound_1 = max(upper_bounds) if upper_bounds else 0
-    print(f"first upper : {uppper_bound_1}")
+    upper_bound_1 = max(upper_bounds) if upper_bounds else 0
+    print(f"first upper : {upper_bound_1}")
     max_distances = [max(D[i][:-1]) for i in range(n)]
     max_distances.sort()
-    upper_bound_2 = sum(max_distances[m:]) + max(D[n]) + max([D[j][n] for j in range(n)])
+    #upper_bound_2 = sum(max_distances[m:]) + max(D[n]) + max([D[j][n] for j in range(n)])
+    upper_bound_2 = sum(max_distances[n-n//m:]) + max(D[n][j] + D[j][n] for j in range(n))
     print(f"second upper : {upper_bound_2}")
-    return min(uppper_bound_1, upper_bound_2)
+    return min(upper_bound_1, upper_bound_2)
 
 
-
-def calculate_lower_bound(n, D):
-
-    # Calculate the lower bound as the maximum of (D[j][n] + D[n][j]) for all items j
-    lower_bound = max(D[j][n] + D[n][j] for j in range(n))
-    return lower_bound
